d91d85b1f1435b6e219be14f454db3272c3a5d61
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2025-07-27 13:54:07 -0300

    feat: read .env vars and listen and serve

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/DevOps-Group-D/YouToFy-API
 
 go 1.24.5
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	// Read .env variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	router := chi.NewRouter()
+
+	fmt.Println("Listening and serving on localhost:3333")
+	http.ListenAndServe(":3333", router)
 }
