5d1490c3545daabeea5567e809cc93aef5ad9b89
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2025-07-27 22:06:13 -0300

    feat: add connect database function

---
+++ database/connection.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/DevOps-Group-D/YouToFy-API/configs"
+	_ "github.com/lib/pq"
+)
+
+func Connect() (*sql.DB, error) {
+	cfg := configs.Cfg
+
+	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.DBConfig.Host, cfg.DBConfig.Port, cfg.DBConfig.User, cfg.DBConfig.Password, cfg.DBConfig.Name, cfg.DBConfig.SslMode)
+	fmt.Println(sc)
+
+	conn, err := sql.Open("postgres", sc)
+	if err != nil {
+		return nil, err
+	}
+
+	err = conn.Ping()
+
+	return conn, err
+}
