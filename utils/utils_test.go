ef1b9fa38a5f2bb300100b5e674746f162e18e7f
Guilherme.Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2025-08-21 14:03:52 -0300

    test: password tests with different strings

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -19,6 +19,55 @@ func TestHash(t *testing.T) {
 	}
 }
 
+func TestHashWithEmptyPassword(t *testing.T) {
+	password := ""
+	hashedPassword, err := HashPassword(password)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if hashedPassword == "" {
+		t.Error("expected hashed password to be non-empty")
+	}
+	err = CheckHashedPassword(hashedPassword, password)
+	if err != nil {
+		t.Errorf("expected no error when checking hashed password, got %v", err)
+	}
+}
+
+func TestCheckHashedPasswordWithWrongPassword(t *testing.T) {
+	password := "testpassword"
+	wrongPassword := "wrongpassword"
+	hashedPassword, err := HashPassword(password)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	err = CheckHashedPassword(hashedPassword, wrongPassword)
+	if err == nil {
+		t.Error("expected error when checking wrong password, got nil")
+	}
+}
+
+func TestCheckHashedPasswordWithEmptyHash(t *testing.T) {
+	password := "testpassword"
+	emptyHash := ""
+	err := CheckHashedPassword(emptyHash, password)
+	if err == nil {
+		t.Error("expected error when checking empty hash, got nil")
+	}
+}
+
+func TestCheckHashedPasswordWithEmptyPassword(t *testing.T) {
+	emptyPassword := ""
+	hashedPassword, err := HashPassword(emptyPassword)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	err = CheckHashedPassword(hashedPassword, emptyPassword)
+	if err != nil {
+		t.Errorf("expected no error when checking empty password, got %v", err)
+	}
+}
+
 func TestToken(t *testing.T) {
 	token, err := GenerateToken(32)
 	if err != nil {
